86f9a1385dd6b1d97e7cbfbcf7759460cc7ce852
mandrianina <mrazaf98@gmail.com>
2016-08-23 09:37:53 +0300

    Base fonctionnelle

--- src/Batis/CoreBundle/Controller/CoreController.php
+++ src/Batis/CoreBundle/Controller/CoreController.php
@@ -4,10 +4,10 @@ namespace Batis\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class CoreController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BatisCoreBundle:Default:index.html.twig');
+        return $this->render('BatisCoreBundle:Core:index.html.twig');
     }
 }
