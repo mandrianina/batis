148619a40e37f72238f69a147b8c8fb4882352d6
mandrianina <mrazaf98@gmail.com>
2016-08-23 11:46:56 +0300

    ajout formulaire dans lancer votre projet

--- src/Batis/CoreBundle/Controller/CoreController.php
+++ src/Batis/CoreBundle/Controller/CoreController.php
@@ -3,6 +3,9 @@
 namespace Batis\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Batis\CrowdsourcingBundle\Entity\Project;
+use Batis\CrowdsourcingBundle\Form\ProjectType;
+use Symfony\Component\HttpFoundation\Request;
 
 class CoreController extends Controller
 {
@@ -11,8 +14,27 @@ class CoreController extends Controller
         return $this->render('BatisCoreBundle:Core:index.html.twig');
     }
 
-    public function addProjectAction()
+    public function addProjectAction(Request $request)
     {
-    	return $this->render('BatisCoreBundle:Core:addProject.html.twig');
+    	$em = $this->getDoctrine()->getManager();
+
+    	$projet = new Project();
+
+    	$form = $this->createForm(ProjectType::class, $projet);
+
+    	if($request->isMethod('POST'))
+    	{
+    		$form->handleRequest($request);
+
+    		if($form->isValid())
+    		{
+    			
+    			$em->persist($projet);
+
+    			return $this->redirectToRoute('batis_crowdsourcing_homepage');
+    		}
+    	}
+    	return $this->render('BatisCoreBundle:Core:addProject.html.twig', array('form' => $form->createView() ));
+
     }
 }
